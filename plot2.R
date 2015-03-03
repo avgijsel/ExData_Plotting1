bfb710f6c14cd4537b41489c166c4186e8b0b80e
avgijsel <basurayspam@yahoo.es>
2015-03-03 22:04:57 +0100

    updated plot1.R and added plot & code for plot2
    plot1.R - removed + in code (for continued statements on next line)
    plot2.R - added code to produce the second plot showing the variation
    per minute of the global active power for the two days
    plot2.png - result

--- plot2.R
+++ plot2.R
@@ -6,9 +6,9 @@ setwd(directory)
 # open the power consumption file 
 # start reading around the beginning of February 2007 and skip the rest
 data<-read.csv('household_power_consumption.txt', header=FALSE, sep=';', 
-  na.strings='?',skip=6e4, nrows=1e4, col.names=c('Date','Time',
-  'Global_active_power','Global_reactive_power','Voltage','Global_intensity',
-  'Sub_metering_1','Sub_metering_2','Sub_metering_3'))
+          na.strings='?',skip=6e4, nrows=1e4, col.names=c('Date','Time',
+          'Global_active_power','Global_reactive_power','Voltage',
+          'Global_intensity','Sub_metering_1','Sub_metering_2','Sub_metering_3'))
 
 # convert dates and times
 data$DateTime <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
@@ -18,8 +18,7 @@ data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 selected_days <- (data$Date >= "2007-02-01" & data$Date <="2007-02-02")
 data <- subset(data,selected_days)
 
-# create a PNG with the histogram of global active power using red bars and annotate it
-png(filename = "plot1.png",width = 480, height = 480, units = "px")
-with(data,{hist(Global_active_power, main='Global Active Power',
-                xlab='Global Active Power (kilowatts)', col='red')})
+# create a PNG with a plot of the global active power versus weekdays
+png(filename = "plot2.png",width = 480, height = 480, units = "px")
+with(data,{plot(data$DateTime, Global_active_power, type="l",xlab='', ylab='Global Active Power (kilowatts)')})
 dev.off()
