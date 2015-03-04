080170c07badb0cd69360769e6794d275f5a879a
avgijsel <basurayspam@yahoo.es>
2015-03-04 14:03:16 +0100

    updated comment for plot 3 + code for (and) plot 4
    updated the description of what is done to create plot 3
    created code to plot plot4
    output: plot4

--- plot4.R
+++ plot4.R
@@ -18,10 +18,15 @@ data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 selected_days <- (data$Date >= "2007-02-01" & data$Date <="2007-02-02")
 data <- subset(data,selected_days)
 
-# create a PNG with a plot of the sub meter power readings versus weekdays
-png(filename = "plot3.png",width = 480, height = 480, units = "px")
+# create a PNG with four subplots
+png(filename = "plot4.png",width = 480, height = 480, units = "px")
+par(mfrow=c(2,2))
+with(data,{plot(data$DateTime, Global_active_power, type="l",xlab='', ylab='Global Active Power')})
+with(data,{plot(data$DateTime, data$Voltage, type="l",xlab='datetime', ylab='Voltage')})
 with(data,{plot(data$DateTime, Sub_metering_1, type="l",col="black",xlab='', ylab='Energy sub metering')})
 lines(data$DateTime, data$Sub_metering_2, col="red")
 lines(data$DateTime, data$Sub_metering_3, col="blue")
 legend("topright",c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+with(data,{plot(data$DateTime, data$Global_reactive_power, type="l",xlab='datetime', ylab='Global_reactive_power')})
+
 dev.off()
